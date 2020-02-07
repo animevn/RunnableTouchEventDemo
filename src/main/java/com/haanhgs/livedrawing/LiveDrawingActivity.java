0fd0d2576f34fe61fc9e00239ac9f074ec0f1fe1
animevn <haanhth21@gmail.com>
2020-02-07 09:46:15 +0700

    reset

--- src/main/java/com/haanhgs/livedrawing/LiveDrawingActivity.java
+++ src/main/java/com/haanhgs/livedrawing/LiveDrawingActivity.java
@@ -1,35 +1,25 @@
 package com.haanhgs.livedrawing;
 
 import android.app.Activity;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Display;
 
 public class LiveDrawingActivity extends Activity {
-    private LiveDrawingView liveDrawingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        liveDrawingView = new LiveDrawingView(this, size.x, size.y);
-        setContentView(liveDrawingView);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        liveDrawingView.resume();
 
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        liveDrawingView.pause();
 
     }
 }
