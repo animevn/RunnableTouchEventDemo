9ec97894ca554372d0acc3a6a24993a15c22c256
animevn <haanhth21@gmail.com>
2020-02-09 08:41:43 +0700

    reset

--- src/main/java/com/haanhgs/livedrawing/LiveDrawingActivity.java
+++ src/main/java/com/haanhgs/livedrawing/LiveDrawingActivity.java
@@ -3,27 +3,21 @@ package com.haanhgs.livedrawing;
 import android.app.Activity;
 import android.os.Bundle;
 
-//test
 public class LiveDrawingActivity extends Activity {
 
-    private DrawingView drawingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        drawingView = new DrawingView(this);
-        setContentView(drawingView);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        drawingView.resume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        drawingView.pause();
     }
 }
